1c8abd6af3dceb8e962f4a0ac9e218d091d29f73
AyaDmaidi <92691434+AyaDmaidi@users.noreply.github.com>
2022-05-28 19:48:50 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,9 @@
 const express=require("express");
 const router=express.Router();
 
-
 const authController=require('../controllers/auth');
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 router.post('/logout', authController.register);
-
-
-
 
 module.exports=router;
