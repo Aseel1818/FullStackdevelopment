b04527ab65f9cadb4297e6e7fbffd81f3459a6de
AyaDmaidi <92691434+AyaDmaidi@users.noreply.github.com>
2022-05-28 20:01:42 +0300

    Update check similarity of password.js

--- check similarity of password.js
+++ check similarity of password.js
@@ -3,7 +3,6 @@ let value2= document.getElementById("pass").value;
 let element= document.getElementById("b");
  element.addEventListener("click", function(){ 
  if ( document.getElementById("password").value != document.getElementById("pass").value ){
-alert("Repeat Password Well");
-
+alert("Repeat Password Well"); 
 }
   });
