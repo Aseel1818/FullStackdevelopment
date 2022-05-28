add10c85b62b9c4bed945fe131fda19c6e51969d
AyaDmaidi <92691434+AyaDmaidi@users.noreply.github.com>
2022-05-28 19:55:01 +0300

    Update CheckoutSteps.js

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -7,6 +7,6 @@ export default function CheckoutSteps(props) {
       <div className={props.step2 ? 'active' : ''}>Shipping</div>
       <div className={props.step3 ? 'active' : ''}>Payment</div>
       <div className={props.step4 ? 'active' : ''}>Place Order</div>
-    </div>
-  );
+    </div> 
+  ); 
 }
