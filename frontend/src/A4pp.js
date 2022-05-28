4d51184ed3d763fa10fcc3f6329399872eca50ec
Danaaldajane <100998246+Danaaldajane@users.noreply.github.com>
2022-05-28 21:28:03 +0300

    Create A4pp.js

---
+++ frontend/src/A4pp.js
@@ -0,0 +1,30 @@
+@@ -16,6 +16,7 @@ import SigninScreen from './screens/SigninScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import SignupScreen from './screens/SignupScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+
+function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+@@ -79,6 +80,7 @@ function App() {
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route
+                path="/shipping"
+                element={<ShippingAddressScreen />}
+              ></Route>
+              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App;
